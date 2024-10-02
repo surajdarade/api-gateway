16ad05f98fa14079d4da6e2f7a01bff794290f1b
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-02 18:30:05 +0530

    check health for instance

--- utils/loadBalancer.ts
+++ utils/loadBalancer.ts
@@ -29,17 +29,15 @@ loadBalancer.ROUND_ROBIN = async (service: Service): Promise<number> => {
     `Checking if instance ${service.instances[newIndex].url} is enabled`
   );
 
-  const enabledIndex = await loadBalancer.isEnabled(service, newIndex);
+  const enabledIndex = await loadBalancer.isEnabled(service);
   return enabledIndex;
 };
 
-loadBalancer.isEnabled = async (
-  service: Service,
-  index: number
-): Promise<number> => {
+loadBalancer.isEnabled = async (service: Service): Promise<number> => {
+  const index = service.index;
   const instance = service.instances[index];
 
-  if (await checkHealth(index)) {
+  if (await checkHealth(instance)) {
     return index;
   } else {
     instance.enabled = false; // Mark the instance as down
@@ -48,11 +46,37 @@ loadBalancer.isEnabled = async (
       const nextIndex = (index + i + 1) % service.instances.length;
 
       if (await checkHealth(service.instances[nextIndex])) {
-        return nextIndex; // Next Health Index
+        return nextIndex; // Return the next healthy instance
       }
     }
   }
-  return -1;
+  return -1; // No healthy instances found
+};
+
+const checkHealth = async (instance: Instance): Promise<boolean> => {
+  let attempts = 0;
+
+  for (const path of instance.healthCheckPaths) {
+    try {
+      const response = await axios.get(`${instance.url}${path}`);
+      if (response.status === 200) {
+        return true; // Instance is healthy
+      }
+    } catch (error: any) {
+      attempts++;
+      console.error(
+        `Health check failed for ${instance.url + path} :`,
+        error.message
+      );
+      if (attempts >= MAX_RETRIES) {
+        console.error(
+          `Max retries reached for ${instance.url}. Marking as down.`
+        );
+        return false; // Mark as down after maximum retries
+      }
+    }
+  }
+  return false; // None of the health checks passed
 };
 
 export default loadBalancer;
