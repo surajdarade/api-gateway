3a76f04cdff45e5e52379e53e6d347a7b94f76a9
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-03 10:33:34 +0530

    get orders api from orders microservice

--- microservices/order-service/server.ts
+++ microservices/order-service/server.ts
@@ -1,1 +1,33 @@
-console.log("Order Service")
+import express, { Request, Response } from "express";
+import axios from "axios";
+
+const PORT = process.env.PORT || 3001;
+const HOST = "order-service";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/orders", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const response = await axios.get(
+      "https://66a0d43e7053166bcabd025a.mockapi.io/api/v1/orders"
+    );
+
+    if (response.status >= 200 && response.status < 300) {
+      res.status(200).json(response.data);
+    } else {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("Error fetching orders:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => {
+  const authString = "surajdarade:surajdarade";
+  const encodedAuthString = Buffer.from(authString, "utf-8").toString("base64");
+  console.log(`Encoding authstring: ${encodedAuthString}`);
+  console.log(`Order service running on port ${PORT}`);
+});
