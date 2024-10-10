d9edb80edf905d5ea502f1a7fe5dba9e22a44d4a
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-10 10:16:01 +0530

    merging fixes

--- microservices/product-service/server.ts
+++ microservices/product-service/server.ts
@@ -24,6 +24,14 @@ app.get("/products", async (req: Request, res: Response): Promise<void> => {
   }
 });
 
+app.listen(PORT, () => {
+  const authString = "surajdarade:surajdarade";
+  const encodedAuthString = Buffer.from(authString, "utf-8").toString("base64");
+  console.log(`Encoding authstring: ${encodedAuthString}`);
+  console.log(`Product service running on port ${PORT}`);
+  registerService(encodedAuthString);
+});
+
 const registerService = async (encodedAuthString: string): Promise<void> => {
   console.log("Attempting to register service with the gateway...");
   try {
@@ -55,11 +63,3 @@ const registerService = async (encodedAuthString: string): Promise<void> => {
     setTimeout(() => registerService(encodedAuthString), 5000);
   }
 }
-
-app.listen(PORT, () => {
-  const authString = "johndoe:password";
-  const encodedAuthString = Buffer.from(authString, "utf-8").toString("base64");
-  console.log(`Encoding authstring: ${encodedAuthString}`);
-  console.log(`Product service running on port ${PORT}`);
-  registerService(encodedAuthString);
-});
